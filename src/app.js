19ab3c96959cbb4d8e6e984e29c276c4a612ec69
Ashlee Ringler <ashleeringler@gmail.com>
2019-01-04 12:05:47 -0700

    Tweaked server config

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import Dashboard from './components/Dashboard';
 import Footer from './components/Footer';
 import 'normalize.css/normalize.css'; //use normalize to do a css-reset. Makes base styles the same across all browsers
-import '../public/styles.css'; //inject css styles into the dom using webpack
+import './styles/styles.scss'; //inject css styles into the dom using webpack
 
 const jsx = (
     <div>
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,12 @@
 //NODE SCRIPT
 
 const path = require('path');  //node function
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
 
 module.exports = (env) => {
     const isProduction = env === 'production';
+    const CSSExtract = new ExtractTextPlugin('styles.css');
 
     return {
 
@@ -17,15 +20,29 @@ module.exports = (env) => {
                 loader: 'babel-loader',
                 test: /\.js/,   //regular expression to grab any files that end in .js and run them through babel
                 exclude: /node_modules/   //Don't run babel in this folder
-            }, { //sets up webpack to work with css files
-                test: /\.s?css$/,   //regexp that makes the s optional
-                use: [
-                    'style-loader',
-                    'css-loader',
-                    'sass-loader'
-                ]
+            }, {
+                test: /\.s?css$/,
+                use: CSSExtract.extract({
+                    use: [
+                        {
+                            loader: 'css-loader',
+                            options: {
+                                sourceMap: true
+                            }
+                        },
+                        {
+                            loader: 'sass-loader',
+                            options: {
+                                sourceMap: true
+                            }
+                        }
+                    ]
+                })
             }]
         },
+        plugins: [
+            CSSExtract
+        ],
         devtool: isProduction ? 'source-map' : 'inline-source-map', //source map, shows errors exactly where they are in their source file. Makes debugging faster. Supported by all browsers
         devServer: {
             contentBase: path.join(__dirname, 'public')
