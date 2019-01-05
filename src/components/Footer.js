3e75886af89930a721c7eb0f304a81a9e43dc31b
Ashlee Ringler <ashleeringler@gmail.com>
2019-01-05 14:21:53 -0700

    Added attribution to Dark Sky API

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,6 +3,8 @@ import React from 'react';
 const Footer = () => (
     <footer className="footer">
         <p>&copy;2019 by Ash Ringler</p>
+        <br></br>
+        <a href="https://darksky.net/poweredby/"><img src="/images/poweredby.png"></img></a>
     </footer>
 );
 
